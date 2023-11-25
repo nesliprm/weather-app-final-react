4e80a39a48f79fa010e88542212023ff75311224
nesliprm <nesliprm@gmail.com>
2023-11-25 13:42:45 +0100

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer container">
+      <footer>
+        <a
+          href="https://github.com/nesliprm/weather-app-final-react"
+          target="_blank"
+          rel="noreferrer"
+          class="link-dark"
+        >
+          Open Source Code
+        </a>
+        by ▼ Nesli Parmaksizoglu
+      </footer>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Weather />
+    <Footer />
   </React.StrictMode>
 );
 

b504bf518f8e12f23eb7faa1bec6c197b149a8ae
nesliprm <nesliprm@gmail.com>
2023-12-02 22:39:51 +0100

    added axios forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,8 +1,19 @@
 import React from "react";
+import axios from "axios";
 import Icon from "./Icon";
 import "./Forecast.css";
 
 export default function Forecast() {
+  let apiKey = "894a2e7aa7f46eeca5d8778f6faa5a5b";
+  let lat = "52.3676";
+  let lon = "4.9041";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(showForecast);
+
+  function showForecast(response) {
+    console.log(response);
+  }
+
   return (
     <div className="Forecast mt-2">
       <div className="row">
